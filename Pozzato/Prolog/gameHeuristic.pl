8ec0d3a96e92787c9fb40d8538054e336e2f6aa4
Giulia <monticone.giulia91@gmail.com>
2018-08-13 16:29:29 +0200

    manhattan per gioco dell'8
    euristica manhattan per il gioco dell'8 funzionante

--- Pozzato/Prolog/gameHeuristic.pl
+++ Pozzato/Prolog/gameHeuristic.pl
@@ -66,32 +66,33 @@ nth([_|Tail],Pos,X):-
 % swap(Lista,Pos1,Pos2,NuovaLista)
 
 swap(Lista,Pos1,Pos2,NuovaLista):-
-  nth(Lista,Pos1,X1),
-  nth(Lista,Pos2,X2),
-  setElement(Lista,Pos2,X1,Temp),
-  setElement(Temp,Pos1,X2,NuovaLista).
+  nth(Lista,Pos1,casella(N,X1,Y1)),
+  nth(Lista,Pos2,casella(M,X2,Y2)),
+  setElement(Lista,Pos2,casella(N,X1,Y1),Temp),
+  setElement(Temp,Pos1,casella(M,X2,Y2),NuovaLista).
 
 % setElement(Lista,Posizione,Valore,NuovaLista)
 
-setElement([_|Tail],0,X,[X|Tail]):-!.
-setElement([Head|Tail],Pos,X,[Head|NuovaTail]):-
+setElement([casella(_,X1,Y1)|Tail],0,casella(M,_,_),[casella(M,X1,Y1)|Tail]):-!.
+setElement([Head|Tail],Pos,casella(N,X,Y),[Head|NuovaTail]):-
   Pos1 is Pos-1,
-  setElement(Tail,Pos1,X,NuovaTail).
+  setElement(Tail,Pos1,casella(N,X,Y),NuovaTail).
 
 % euristica per azioni
 manhattan([casella(N,X1,Y1)|StatoS],[casella(M,X2,Y2)|StatoF],F,C):-
   manhattan1([casella(N,X1,Y1)|StatoS],[casella(M,X2,Y2)|StatoF],F,C),
   F is C.
 
-manhattan1([],[_],_,C):- C is 0.
-manhattan1([casella(N,X1,Y1)|StatoS],[casella(N,X2,Y2)|StatoF],F,C1):-
+manhattan1([],[_|_],_,C):-
+  C is 0.
+manhattan1([casella(N,X1,Y1)|StatoS],[casella(N,X2,Y2)|_],F,C1):-
   calcolo(X1,X2,Y1,Y2,Res),
   finale(StatoF2),
   manhattan1(StatoS,StatoF2,F,C),
   C1 is C + Res.
 
-manhattan1([casella(N,X1,Y1)|StatoP],[_|StatoF],F,_):-
-  manhattan1([casella(N,X1,Y1)|StatoP],StatoF,F,_).
+manhattan1([casella(N,X1,Y1)|StatoP],[_|StatoF],F,C):-
+  manhattan1([casella(N,X1,Y1)|StatoP],StatoF,F,C).
 
 calcolo(X1,X2,Y1,Y2,Res):-
   Res is abs(X1-X2)+ abs(Y1-Y2).
